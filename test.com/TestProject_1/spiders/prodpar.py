a3c9d3d1dcd246ac826d1392a5a821fe1e25b5a1
Maksim <stik9935@gmail.com>
2022-06-14 16:45:29 +0300

    Parsing product

---
+++ test.com/TestProject_1/spiders/prodpar.py
@@ -0,0 +1,17 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.item import Item, Field
+from TestProject_1.items import Product
+
+
+class ProdparSpider(scrapy.Spider):
+    name = 'prodpar'
+    allowed_domains = ['https://www.kfc.ru/restaurants/']
+    start_urls = ['https://www.kfc.ru/restaurants/1251']
+
+    # rules = (Rule(LinkExtractor(allow='prod',), deny=('/filter', '/?arrFilter', '/?register=', '/auth/', '/?backurl=', '/?forgot_password', '/?VIEW=LINE', '/VIEW=TABLE', '/VIEW=',)), ),)
+
+    def parse(self, response):
+        item = Product()
+        item['product_url'] = response.url
